482ccb695cd4cd012e98be6dc598fcbe541fd2ff
sbkrish <ranimaniyan@gmail.com>
2022-12-04 19:46:41 +0530

    navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { Container } from '@mui/system';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav'
+import Trending from './pages/Trending/Trending';
+import Movies from './pages/Movies/Movies';
+import TvSeries from './pages/TvSeries/TvSeries';
+import Search from './pages/Search/Search';
 
 function App() {
   return (
-    <div className="App">
-      <span>📽️ Entertainment Hub 🎥️</span>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending />} exact/>
+            <Route path='/Movies' element={<Movies />} />
+            <Route path='/tvseries' element={<TvSeries />} />
+            <Route path='/Search' element={<Search />} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
+    
   );
 }
 
